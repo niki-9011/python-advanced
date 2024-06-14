400a2a4b78d09ef7d275452864377e06917137fc
Nikolay Videnov <niki901115@gmail.com>
2024-06-14 23:56:38 +0300

    add task list_manipulator.py

---
+++ exam_preparation/list_manipulator.py
@@ -0,0 +1,21 @@
+def list_manipulator(some_list, command, side, *args):
+    if command == 'remove':
+        quantity = args[0] if args else 1
+        if side == 'end':
+            some_list = some_list[:-quantity]
+        elif side == 'beginning':
+            some_list = some_list[quantity:]
+    elif command == 'add':
+        some_list = some_list + list(args) if side == 'end' else list(args) + some_list
+    return some_list
+
+
+
+print(list_manipulator([1, 2, 3], "remove", "end"))
+print(list_manipulator([1, 2, 3], "remove", "beginning"))
+print(list_manipulator([1, 2, 3], "add", "beginning", 20))
+print(list_manipulator([1, 2, 3], "add", "end", 30))
+print(list_manipulator([1, 2, 3], "remove", "end", 2))
+print(list_manipulator([1, 2, 3], "remove", "beginning", 2))
+print(list_manipulator([1, 2, 3], "add", "beginning", 20, 30, 40))
+print(list_manipulator([1, 2, 3], "add", "end", 30, 40, 50))
